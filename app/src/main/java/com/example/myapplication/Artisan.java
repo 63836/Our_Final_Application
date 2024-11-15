a2153519f41eabffe9f83aba5ca9fd693287375c
63836 <106800566+63836@users.noreply.github.com>
2024-11-16 01:43:13 +0800

    new files

--- app/src/main/java/com/example/myapplication/Artisan.java
+++ app/src/main/java/com/example/myapplication/Artisan.java
@@ -1,6 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +16,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Artisan extends AppCompatActivity {
 
+    ImageView points_section;
+    ImageView Marketplace;
+    int[] images = {R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic, R.drawable.hd_man_user_illustration_icon_transparent_png_701751694974843ybexneueic};
+
+    String[] version = {"Leo", "Nemo", "Ben", "Ted", "Jake", "Finn", "Ash", "Dan", "Liam", "Noah", "Oliver", "William", "James", "Logan"};
+
+    String[] versionNumber = {"Painter", "Sculptor", "Painter", "Designer", "Creator", "Inventor", "Painter", "Sculptor", "Painter", "Designer", "Creator", "Inventor", "Painter", "Sculptor"};
+
+    ListView lView;
+
+    ListAdapter lAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +36,33 @@ public class Artisan extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Marketplace = findViewById(R.id.imageView34);
+        Marketplace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Marketplace.class);
+                startActivity(intent);
+            }
+        });
+        points_section = findViewById(R.id.imageView31);
+        points_section.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Home_Page.class);
+            startActivity(intent);
+        });
+        lView = (ListView) findViewById(R.id.androidList);
+
+        lAdapter = new ListAdapter(getApplicationContext(), version, versionNumber, images);
+
+        lView.setAdapter(lAdapter);
+
+        lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(getApplicationContext(), version[i]+" "+versionNumber[i], Toast.LENGTH_SHORT).show();
+
+            }
         });
     }
 }
--- app/src/main/java/com/example/myapplication/Points_Exchange.java
+++ app/src/main/java/com/example/myapplication/Points_Exchange.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,41 +13,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Marketplace extends AppCompatActivity {
-    ImageView points_section;
-    ImageView Artisan;
-    ImageView Points_Exchange;
+public class Points_Exchange extends AppCompatActivity {
+    Button completed_transaction;
+    ImageView Marketplace;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_marketplace);
+        setContentView(R.layout.activity_points_exchange);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        points_section = findViewById(R.id.imageView23);
-        points_section.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), Home_Page.class);
-            startActivity(intent);
-        });
-        Artisan = findViewById(R.id.imageView25);
-        Artisan.setOnClickListener(new View.OnClickListener() {
+        completed_transaction = findViewById(R.id.button3);
+        completed_transaction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Artisan.class);
+                Intent intent = new Intent(getApplicationContext(), completed_transaction.class);
+                startActivity(intent);
+        }});
+        Marketplace = findViewById(R.id.imageView29);
+        Marketplace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Marketplace.class);
                 startActivity(intent);
             }
         });
-        Points_Exchange = findViewById(R.id.imageView16);
-        Points_Exchange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Points_Exchange.class);
-                startActivity(intent);
-            }
-        });
+
+
     }
 }
